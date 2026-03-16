2bc85c00998dfb356574e1d91be7e670a6bd0162
Jeronimo Aguirre Ortega <jerofx25@gmail.com>
2026-03-16 18:49:42 +0100

    Creacion de Mapper de Workup, su datasource y repository

---
+++ lib/features/workout/infrastructure/datasources/workups_firestore_datasource.dart
@@ -0,0 +1,21 @@
+import 'package:IAEntrenar/models/workup_day.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class WorkupsFirestoreDatasource {
+
+  WorkupsFirestoreDatasource({
+    FirebaseFirestore? firestore,
+  }) : _firestore = firestore ?? FirebaseFirestore.instance;
+
+  final FirebaseFirestore _firestore;
+  static const String _collectionName = "workups";
+
+  Future<List<WorkupDay>> getWorkupDays() async {
+    final snap = await _firestore.collection(_collectionName).get();
+    final list = snap.docs
+        .map((doc) => WorkupDay.fromJson(doc.data()))
+        .toList();
+    list.sort((a, b) => a.dayNumber.compareTo(b.dayNumber));
+    return list;
+  }
+}
